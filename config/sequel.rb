9c2f082664ccdc0331899d896f0596d36cebe6d2
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-06-19 12:23:31 +0100

    Renaming the repo

--- config/sequel.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'sequel'
-
-db_name = 'new_bookmark_manager'
-db_name += '_test' if ENV['RACK_ENV'] == 'test'
-DB_NAME = db_name
-
-DB = Sequel.postgres(DB_NAME)
