0753d60c5bbf24446eb49e36a4a7e9125e1ccce7
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-06-20 12:18:27 +0100

    Using composed matchers. Placing canned data to slime the test before implementing the actual behaviour.

--- app/api.rb
+++ app/api.rb
@@ -4,7 +4,10 @@ require 'sinatra/base'
 require 'json'
 
 module ExpenseTracker
+  # Routing code for expenses creation and look-up.
   class API < Sinatra::Base
-
+    post '/expenses' do
+      JSON.generate('expense_id': 42)
+    end
   end
 end
