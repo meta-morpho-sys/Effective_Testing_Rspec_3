68a80d5d52418ec34c4499c3c0c7bf2890bed8e9
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-06-20 14:42:55 +0100

    Extracting 'posting expenses' logic into a helper method. Adding tests for getting an expense from out web service

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -5,6 +5,7 @@ require 'json'
 require_relative '../../app/api'
 
 module ExpenseTracker
+
   describe 'ExpenseTracker API' do
     include Rack::Test::Methods
 
@@ -12,19 +13,41 @@ module ExpenseTracker
       ExpenseTracker::API.new
     end
 
+    def record_to_hash
+      JSON.parse(last_response.body)
+    end
+
+    def post_expense(expense)
+      post '/expenses', JSON.generate(expense)
+      expect(last_response.status).to eq 200
+
+      parsed = record_to_hash
+      expect(parsed).to include('expense_id' => a_kind_of(Integer))
+      expense.merge('id' => parsed['expense_id'])
+    end
+
     it 'records submitted expenses' do
-      coffee = {
+      coffee = post_expense(
         'payee': :Starbucks,
         'amount': 5.75,
         'date': '2017-06-10'
-      }
-      # Verifying that the POST request ONLY completes without crashing the app.
-      post '/expenses', JSON.generate(coffee)
+      )
+      zoo = post_expense(
+        'payee': :Zoo,
+        'amount': 15.25,
+        'date': '2017-06-10'
+      )
+      groceries = post_expense(
+        'payee': :'Whole Foods',
+        'amount': 95.20,
+        'date': '2017-06-11'
+      )
 
+      get '/expenses/2017-06-10'
       expect(last_response.status).to eq 200
-
-      parsed = JSON.parse(last_response.body)
-      expect(parsed).to include('expense_id' => a_kind_of(Integer))
+      expenses = JSON.parse(last_response.body)
+      expect(expenses).to contain_exactly coffee, zoo
+      expect(expenses).to_not include groceries
     end
   end
 end
