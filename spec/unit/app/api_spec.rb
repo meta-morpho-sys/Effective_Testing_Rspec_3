6d57a9c5c897cba81b3f92f97fb76b89c0b1a5b7
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-06-21 13:20:30 +0100

    Returning expense id after HTTP request. Unit test driven by RACK::TEST

---
+++ spec/unit/app/api_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require_relative '../../../app/api'
+
+module ExpenseTracker
+  RecordResult = Struct.new(:success?, :expense_id, :error_message)
+
+
+  describe API do
+
+    def app
+      API.new(ledger: ledger)
+    end
+
+    let(:ledger) { instance_double('ExpenseTracker::Ledger') }
+
+    describe "POST '/expenses '" do
+      context 'when the expense is successfully recorded' do
+        it 'returns the expense id' do
+          expense = { 'some' => 'data' }
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(true, 417, nil))
+
+          post '/expenses', JSON.generate(expense)
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to include('expense_id' => 417)
+        end
+
+        it 'responds with a 200(OK)'
+      end
+
+      context 'when the expense fails validation' do
+        it 'returns an error message'
+        it 'responds with a 422 (Unprocessable entity'
+      end
+    end
+  end
+end
