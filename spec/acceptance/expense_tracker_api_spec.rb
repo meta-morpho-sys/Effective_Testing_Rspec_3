063682b9439de549d0a938785d65c08d4b190f70
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-06-25 01:17:57 +0100

    Symbolized names while parsing data with JSON

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -14,7 +14,7 @@ module ExpenseTracker
     end
 
     def record_to_hash
-      JSON.parse(last_response.body)
+      JSON.parse(last_response, symbolize_names: true)
     end
 
     def post_expense(expense)
@@ -28,24 +28,24 @@ module ExpenseTracker
 
     it 'records submitted expenses' do
       coffee = post_expense(
-        'payee' => 'Starbucks',
-        'amount' => 5.75,
-        'date' => '2017-06-10'
+        payee: 'Starbucks',
+        amount:  5.75,
+        date:  '2017-06-10'
       )
       zoo = post_expense(
-        'payee'  => 'Zoo',
-        'amount' => 15.25,
-        'date' => '2017-06-10'
+        payee: 'Zoo',
+        amount:  15.25,
+        date:  '2017-06-10'
       )
       groceries = post_expense(
-        'payee' => 'Whole Foods',
-        'amount' => 95.20,
-        'date' => '2017-06-11'
+        payee: 'Whole Foods',
+        amount:  95.20,
+        date:  '2017-06-11'
       )
 
       get '/expenses/2017-06-10'
       expect(last_response.status).to eq 200
-      expenses = JSON.parse(last_response.body)
+      expenses = JSON.parse(last_response, symbolize_names: true)
       expect(expenses).to contain_exactly coffee, zoo
       expect(expenses).to_not include groceries
     end
